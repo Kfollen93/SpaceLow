06b4022a1255ab8ce9c2f351a9f3f61db40b8c0a
Kyle <59298462+Kfollen93@users.noreply.github.com>
2021-05-06 17:50:21 -0700

    Platforming scripts

---
+++ Platforming/MoveWithPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlatform : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.parent = null;
+        }
+    }
+}
---
+++ Platforming/MovingAsteroids.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAsteroids : MonoBehaviour
+{
+	public Transform pos1, pos2;
+	public float speed;
+	public Transform startPos;
+	private Vector3 nextPos;
+
+	private void Start()
+	{
+		nextPos = startPos.position;
+	}
+
+	private void Update()
+	{
+		if (transform.position == pos1.position)
+		{
+			nextPos = pos2.position;
+		}
+		if (transform.position == pos2.position)
+		{
+			nextPos = pos1.position;
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(pos1.position, pos2.position);
+	}
+}
